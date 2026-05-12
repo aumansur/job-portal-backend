aac093772bb9dee08a97a15f7e7428a5d26cee4c
aumansur <ahammadullah2001@gmail.com>
2026-05-13 00:51:56 +0600

    render setup

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,24 @@ import cors from "cors";
 
 import { globalErrorHandler } from "./middlewares/errorHandler";
 import router from "./routes";
+import { connectDB } from "./config/db";
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Job Portal Backend Running",
+  });
+});
+
 app.use("/api/v1", router);
+
 app.use(globalErrorHandler);
 
 export default app;
